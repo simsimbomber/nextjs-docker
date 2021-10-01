2f19495cc3cb05b211c97687bedbc82c2cc08073
Yusuke Shimura <shimura@systemplanet.co.jp>
2021-10-01 19:43:21 +0900

    一旦コミット

--- components/contents.js
+++ components/contents.js
@@ -5,7 +5,7 @@ import React, { useState, useEffect } from 'react';
 const Contents = () => {
   console.log('contentsコンポーネント読み込み');
 
-  // コンポーネントのマウント時に1回だけ実行する
+  // コンポーネントがレンダーのされる度に実行する
   useEffect( async () => {
     console.log('contentsコンポーネント（useEffect）読み込み');
     // Tweetテーブルから作成日の新しい順に10個のデータ取得
@@ -13,14 +13,13 @@ const Contents = () => {
     console.log(tail10Tweet);
     // 履歴を表示
     drawHistory(tail10Tweet);
-  }, []);
+  });
 
   // DBからツイートデータを全て取得
   const getTail10Tweet= async() => {
     // ,sql:'created_timeAsc10'
     const res = await fetch('http://localhost:3000/api/tweet',{method:'GET'});
     const datas = await res.json();
-    console.log('TTEESSTT');
     console.log(datas.data);
     return datas.data;
 }
@@ -35,9 +34,9 @@ const Contents = () => {
   
     // removeChildren(element)//一番古い履歴を削除(子ノードのリストを削除)
     // slice()は、文字列や配列などからデータの一部分だけ取り出せるメソッドになります
-    // hoge.reverse() で破壊的。hoge.slice().reverse() で非破壊的.逆順にしたhistoryをrecordにいれる
+    // hoge.slice()historyをrecordにいれる
     //「配列」historyの値を1つずつ「変数recordへ代入してくれるようになります。
-    for (const record of history.slice().reverse()) {
+    for (const record of history.slice()) {
       console.log(record);
       const { id, create_user_id, contents, favorite_count, retweet_count, created_time } = record;
 
